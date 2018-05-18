19d8683a71dfb168c1cf3ad464b206a5c522893a
Aisatou Diallo <diallo.aisatou@gmail.com>
2018-05-18 15:56:11 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,4 +27,19 @@ class Song
     @@genres.uniq
   end
   
+  def self.genre_count
+   @@genres.each do |genre|
+      @@genre_count[genre] = @@genres.count(genre)
+    end
+    @@genre_count
+  end
+
+  def self.artist_count
+    @@artists.each do |artist|
+      @@artist_count[artist] = @@artists.count(artist)
+    end
+    @@artist_count
+  end
+end
+  
 end
